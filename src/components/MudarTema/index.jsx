d5507928f4062b8530fbb1209e3aeff5c74075bc
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-06 23:47:00 -0300

    MudarTema

---
+++ src/components/MudarTema/index.jsx
@@ -0,0 +1,11 @@
+import styles from './mudarTema.module.scss';
+
+export default function MudarTema() {
+  return (
+    <div>
+      <button className={styles.mybutton}>
+      <p>j</p>
+      </button>
+    </div>
+  )
+}
